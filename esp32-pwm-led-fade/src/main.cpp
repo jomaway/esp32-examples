6823061fdf329ba7163771c79b20820147b2936b
jomaway <jomaway@users.noreply.github.com>
2026-07-28 16:30:54 +0200

    refactor: use arduino v3 functions for ledc

--- esp32-pwm-led-fade/src/main.cpp
+++ esp32-pwm-led-fade/src/main.cpp
@@ -3,12 +3,19 @@
 // See: https://docs.espressif.com/projects/arduino-esp32/en/latest/migration_guides/2.x_to_3.0.html
 #include <Arduino.h>
 
+// Check if the Arduino ESP32 Core version is 3.x or newer
+// This is important because the ledc functions used in this code
+// are only available in version 3.x and newer.
+static_assert(
+    ESP_ARDUINO_VERSION_MAJOR >= 3,
+    "Please install Arduino ESP32 Core 3.x or newer.");
+
 constexpr uint8_t ledPin = 18;
 constexpr uint8_t fadeDelay = 10;
+constexpr uint32_t delayTime = 500; // Delay time in milliseconds for fade in/out
 
 constexpr uint16_t pwmFrequence = 5000; // PWM frequency in Hz.
 constexpr uint8_t pwmResolution = 8;    // PWM resolution (1-16 bits). 8-bit means 0-255 duty cycle.
-constexpr uint8_t pwmChannel = 0;
 
 void setup()
 {
@@ -16,9 +23,7 @@ void setup()
   Serial.begin(9600);
 
   // config the ledc pwm
-  ledcSetup(pwmChannel, pwmFrequence, pwmResolution);
-  ledcAttachPin(ledPin, pwmChannel);
-
+  ledcAttach(ledPin, pwmFrequence, pwmResolution);
   Serial.println("--- Setup finished ---");
 }
 
@@ -27,18 +32,18 @@ void loop()
   // Fade in (0-255)
   for (int dutyCycle = 0; dutyCycle <= 255; dutyCycle++)
   {
-    ledcWrite(pwmChannel, dutyCycle);
+    ledcWrite(ledPin, dutyCycle);
     delay(fadeDelay); // Adjust for fade speed
   }
 
-  delay(500); // stay on for 500 ms
+  delay(delayTime); // stay on for the specified time
 
   // Fade out (255-0)
   for (int dutyCycle = 255; dutyCycle >= 0; dutyCycle--)
   {
-    ledcWrite(pwmChannel, dutyCycle);
+    ledcWrite(ledPin, dutyCycle);
     delay(fadeDelay); // Adjust for fade speed
   }
 
-  delay(500); // stay off for 1 s
+  delay(delayTime); // stay off for the specified time
 }
--- esp32-pwm-servo/src/main.cpp
+++ esp32-pwm-servo/src/main.cpp
@@ -2,6 +2,12 @@
 // As platformio ships this version by default.
 // See: https://docs.espressif.com/projects/arduino-esp32/en/latest/migration_guides/2.x_to_3.0.html
 #include <Arduino.h>
+// Check if the Arduino ESP32 Core version is 3.x or newer
+// This is important because the ledc functions used in this code
+// are only available in version 3.x and newer.
+static_assert(
+    ESP_ARDUINO_VERSION_MAJOR >= 3,
+    "Please install Arduino ESP32 Core 3.x or newer.");
 
 constexpr uint8_t servoPin = 18;
 constexpr uint8_t potiPin = 35;
@@ -13,8 +19,9 @@ constexpr uint8_t pwmChannel = 0;
 void setup()
 {
   // config the pwm
-  ledcSetup(pwmChannel, pwmFrequence, pwmResolution);
-  ledcAttachPin(servoPin, pwmChannel);
+  ledcAttach(servoPin, pwmFrequence, pwmResolution);
+  // if you want to use a specific channel, you can use the following function instead of ledcAttach():
+  // ledcAttachChannel(servoPin, pwmFrequence, pwmResolution, pwmChannel);
 }
 
 void loop()
@@ -26,5 +33,5 @@ void loop()
   // servo need 2.5% for 0° and 12.5% for 180°
   uint64_t dutyCycle = map(potValue, 0, 4095, 102, 512);
 
-  ledcWrite(pwmChannel, dutyCycle);
+  ledcWrite(servoPin, dutyCycle);
 }
