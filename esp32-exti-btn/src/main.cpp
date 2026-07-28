ab2a5f47a5eaa68173a02a490401dbb3ddd2ef94
jomaway <jomaway@users.noreply.github.com>
2026-07-28 17:15:56 +0200

    feat: add a millis version to improve this example

--- esp32-exti-btn/src/main.cpp
+++ esp32-exti-btn/src/main.cpp
@@ -4,18 +4,25 @@ constexpr uint8_t ledPin = 18;
 constexpr uint8_t btnStartPin = 34;
 constexpr uint8_t btnStopPin = 35;
 
+// we need to declare the running variable as volatile because it is modified inside an ISR
 volatile bool running = false;
+
+// for demonstration purposes, we will use a busy delay of 2 seconds to simulate some work being done
+// this could be some time consuming calculation or similar, but for the sake of this example we will just use a delay
+constexpr uint32_t busyDelay = 2000;
 
 // isr for pressing the start button
 // ! on esp32 the IRAM_ATTR is needed
 void IRAM_ATTR startISR()
 {
+  // only set a flag, don't do any heavy work inside the ISR!
   running = true;
 }
 
 // isr for pressing the stop button
 void IRAM_ATTR stopISR()
 {
+  // only set a flag, don't do any heavy work inside the ISR!
   running = false;
 }
 
@@ -29,18 +36,19 @@ void setup()
   // call our ISR if a falling edge happens on the defined pins.
   attachInterrupt(btnStartPin, startISR, FALLING);
   attachInterrupt(btnStopPin, stopISR, FALLING);
+
+  Serial.println("Press the start button to start the work, and the stop button to stop it.");
 }
 
-void loop()
+void loop_example_1()
 {
   // ! we never check our button state inside the loop
-
   while (running)
   {
     // pretend to do some important work! (blink an led)
     // this could be some time consuming calculation or similar
     digitalWrite(ledPin, !digitalRead(ledPin));
-    delay(300);
+    delay(busyDelay);
     Serial.print('.');
   }
 
@@ -48,4 +56,37 @@ void loop()
   {
     digitalWrite(ledPin, LOW);
   }
+}
+
+void loop_example_2()
+{
+  // static means that the variable is only initialized once,
+  // and keeps its value between calls to loop()
+  static uint32_t lastBlink = 0;
+
+  // ! we never check our button state inside the loop
+  while (running)
+  {
+    // pretend to do some important work! (blink an led)
+    // this could be some time consuming calculation or similar
+
+    if (millis() - lastBlink > busyDelay)
+    {
+      lastBlink = millis();
+      digitalWrite(ledPin, !digitalRead(ledPin));
+      Serial.print('.');
+    }
+  }
+
+  if (!running)
+  {
+    lastBlink = 0;
+    digitalWrite(ledPin, LOW);
+  }
+}
+
+void loop()
+{
+  loop_example_1();
+  // loop_example_2();
 }
